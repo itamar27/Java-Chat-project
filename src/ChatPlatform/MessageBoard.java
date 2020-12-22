d7c54a9112995990d234d9d47c7423ce9007b8f2
barakdaniel <barakdaniel22@gmail.com>
2020-12-22 21:21:59 +0200

    Implemented Server Classses
    ConnectionProxy, ClientDiscriptor, MessageBoard.

--- src/ChatPlatform/MessageBoard.java
+++ src/ChatPlatform/MessageBoard.java
@@ -1,9 +1,31 @@
 package ChatPlatform;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageBoard implements StringConsumer, StringProducer
-
 {
+    List<StringConsumer> consumers = null;
 
- //
+    public MessageBoard() {
+        consumers = new ArrayList<>();
+    }
 
+    @Override
+    public void consume(String str) throws IOException {
+        for(StringConsumer consumer : consumers) {
+            consumer.consume(str);
+        }
+    }
+
+    @Override
+    public void addConsumer(StringConsumer sc) {
+        consumers.add(sc);
+    }
+
+    @Override
+    public void removeConsumer(StringConsumer sc) {
+        consumers.remove(sc);
+    }
 }
